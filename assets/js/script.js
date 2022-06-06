81a987f7fd42dbfc4f37400cad88e99435afe1f3
Revappa <revumallapure@gmail.com>
2022-06-06 14:05:26 +0530

    UPDATE

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+<script>
+
+        let toggle = document.getElementsByClassName('toggle_bar');
+
+        function toggle_bar() {
+
+         let element = document.querySelector(".toggle");
+         element.classList.toggle("show")
+         
+         let element2 = document.querySelector(".bar")
+         element2.classList.toggle("bar2")
+        }
+
+</script>
