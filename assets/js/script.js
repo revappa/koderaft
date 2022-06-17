f58603427c05f4aa1d495074cf49ae6bf864b590
Revappa <revumallapure@gmail.com>
2022-06-17 22:32:36 +0530

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,37 +8,10 @@ function scrollFunction() {
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-});
 
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
+
+$(function(){
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
   });
-});
-
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-document.onkeydown = function (e) {
-  // disable F12 key
-  if (e.keyCode == 123) {
-    return false;
-  }
-
-  // disable I key
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-    return false;
-  }
-
-  // disable J key
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-    return false;
-  }
-
-  // disable U key
-  if (e.ctrlKey && e.keyCode == 85) {
-    return false;
-  }
-};
-
 
