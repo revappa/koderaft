86f76209a01b99cae4edf8f26363ac52b7754146
Revappa <revumallapure@gmail.com>
2022-06-17 21:44:18 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,44 @@
-<script>
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  document.getElementById("navbar").style.background = "#fff";
+}
 
-        let toggle = document.getElementsByClassName('toggle_bar');
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-        function toggle_bar() {
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
 
-         let element = document.querySelector(".toggle");
-         element.classList.toggle("show")
-         
-         let element2 = document.querySelector(".bar")
-         element2.classList.toggle("bar2")
-        }
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
 
-</script>
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.onkeydown = function (e) {
+  // disable F12 key
+  if (e.keyCode == 123) {
+    return false;
+  }
+
+  // disable I key
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+    return false;
+  }
+
+  // disable J key
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+    return false;
+  }
+
+  // disable U key
+  if (e.ctrlKey && e.keyCode == 85) {
+    return false;
+  }
+};
+
+
